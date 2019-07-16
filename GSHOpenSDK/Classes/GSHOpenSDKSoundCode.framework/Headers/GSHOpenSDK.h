ef6e8a1b95c9ae823a83dbe7f3f017b4dbaa1eb7
mayer <gemdale@gemdaledeiMac-2.local>
2019-07-16 15:56:52 +0800

    0.2.2

--- GSHOpenSDK/Classes/GSHOpenSDKSoundCode.framework/Headers/GSHOpenSDK.h
+++ GSHOpenSDK/Classes/GSHOpenSDKSoundCode.framework/Headers/GSHOpenSDK.h
@@ -8,11 +8,14 @@
 
 #import <Foundation/Foundation.h>
 #import "GSHHTTPAPIClient.h"
+#import "GSHWebSocketClient.h"
 
 @interface GSHOpenSDK : NSObject
 @property(nonatomic,strong,readonly)GSHHTTPAPIClient *httpAPIClient;
+@property(nonatomic,strong,readonly)GSHWebSocketClient *webSocketClient;
 
 +(instancetype)share;
 -(void)updateUserId:(NSString*)userId sessionId:(NSString*)sessionId;
 -(void)updateHttpDomain:(NSString*)httpDomain port:(NSNumber*)port;
+-(void)updateGWId:(NSString*)gwId;
 @end
