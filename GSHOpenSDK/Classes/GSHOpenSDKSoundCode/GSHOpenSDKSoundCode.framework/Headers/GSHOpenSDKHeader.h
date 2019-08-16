8fb5a6aef77c5ab0c799a51705b20a2137e666f8
mayer <gemdale@gemdaledeiMac-2.local>
2019-08-16 16:39:16 +0800

    no message

---
+++ GSHOpenSDK/Classes/GSHOpenSDKSoundCode/GSHOpenSDKSoundCode.framework/Headers/GSHOpenSDKHeader.h
@@ -0,0 +1,35 @@
+//
+//  GSHOpenSDKHeader.h
+//  GSHOpenSDKSoundCode
+//
+//  Created by gemdale on 2019/8/16.
+//  Copyright © 2019 gemdale. All rights reserved.
+//
+
+#ifndef GSHOpenSDKHeader_h
+#define GSHOpenSDKHeader_h
+
+#import <GSHOpenSDKSoundCode/GSHOpenSDK.h>
+#import <GSHOpenSDKSoundCode/GSHUserM.h>
+#import <GSHOpenSDKSoundCode/GSHFamilyM.h>
+#import <GSHOpenSDKSoundCode/GSHFamilyMemberM.h>
+#import <GSHOpenSDKSoundCode/GSHFloorM.h>
+#import <GSHOpenSDKSoundCode/GSHRoomM.h>
+#import <GSHOpenSDKSoundCode/GSHGatewayM.h>
+#import <GSHOpenSDKSoundCode/GSHDeviceM.h>
+#import <GSHOpenSDKSoundCode/GSHSensorM.h>
+#import <GSHOpenSDKSoundCode/GSHAutoM.h>
+#import <GSHOpenSDKSoundCode/GSHSceneM.h>
+
+#import <GSHOpenSDKSoundCode/GSHDefenseM.h>
+#import <GSHOpenSDKSoundCode/GSHMessageM.h>
+#import <GSHOpenSDKSoundCode/GSHVoiceM.h>
+#import <GSHOpenSDKSoundCode/GSHVersionM.h>
+
+#import <GSHOpenSDKSoundCode/GSHYingShiManager.h>
+#import <GSHOpenSDKSoundCode/GSHInfraredControllerManager.h>
+
+#import <GSHOpenSDKSoundCode/GSHFileManager.h>
+#import <GSHOpenSDKSoundCode/GSHWebSocketClient.h>
+
+#endif /* GSHOpenSDKHeader_h */
