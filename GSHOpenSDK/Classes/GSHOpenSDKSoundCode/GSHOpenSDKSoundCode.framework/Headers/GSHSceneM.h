a18ba29fc4429dbbba5447c35ed888b099f29669
zhanghong <zhanghong5@gemdale.com>
2019-12-18 17:13:30 +0800

    0.2.104

--- GSHOpenSDK/Classes/GSHOpenSDKSoundCode/GSHOpenSDKSoundCode.framework/Headers/GSHSceneM.h
+++ GSHOpenSDK/Classes/GSHOpenSDKSoundCode/GSHOpenSDKSoundCode.framework/Headers/GSHSceneM.h
@@ -94,7 +94,7 @@
 @interface GSHSceneM : GSHBaseModel 
 
 @property (nonatomic , strong) NSNumber *backgroundId;   //背景图片id
-@property (nonatomic , strong) NSString *picUrl;   //背景图片url
+@property (nonatomic , strong) NSString *backgroundUrl;   //背景图片url
 @property (nonatomic , strong) NSNumber *familyId;   //家庭id
 @property (nonatomic , strong) NSNumber *floorId;   //楼层id
 @property (nonatomic , copy) NSString *floorName;   //楼层名称
